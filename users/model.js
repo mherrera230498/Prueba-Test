024130f0f87a46021529d67b11fe25038b5c0cca
mherrera230498 <139415483+mherrera230498@users.noreply.github.com>
2023-07-12 23:49:45 -0500

    Update npm-publish.yml (#2)
    * Update npm-publish.yml
    
    * Added Eslint
    
    * Update npm-publish.yml
    
    * Update npm-publish.yml

--- users/model.js
+++ users/model.js
@@ -4,21 +4,21 @@ import sequelize from '../shared/database/database.js'
 class User extends Model {}
 
 User.init({
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        dni: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false
-        }
-    },
-    {
-        sequelize,
-        modelName: 'user',
-        timestamps: false
-    }
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  dni: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  }
+},
+{
+  sequelize,
+  modelName: 'user',
+  timestamps: false
+}
 )
 
 export default User
